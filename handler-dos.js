52e3904fde10498582a37e1bfc62760051809852
Jazmin Jahaziel <jazmin.jahazieltru@gmail.com>
2021-07-22 16:48:09 -0400

    function filter and mostrarConsola is complete

--- handler-dos.js
+++ handler-dos.js
@@ -1,5 +1,16 @@
 
 module.exports.mostrarConsola = async (event) => {
     console.log(JSON.stringify(event));
-    return true;
+
+      let arr = event.detail.personas
+
+      function mensaje() {
+        for( i = 0; i < arr.length; i++){
+
+        console.log(`Este es ${arr[i].nombre} y tiene ${arr[i].edad} años`)
+
+        }
+      }
+
+      mensaje();
   };
--- handler.js
+++ handler.js
@@ -7,16 +7,29 @@ module.exports.enviarMensaje = async (event) => {
   return respuesta;
 };
 
-function sendEventBridge(event) {
-  const eventBridge = new AWS.EventBridge({ region: 'us-east-1' }); 
-  return eventBridge.putEvents({
-    Entries: [
-      {
-        EventBusName: 'test',
-        Source: 'mensajeTest',
-        DetailType: 'UserSignUp',
-        Detail: JSON.stringify(event)
-    },
-    ]
-  }).promise()
- }
+try {
+  
+  function sendEventBridge(event) {
+    const eventBridge = new AWS.EventBridge({ region: 'us-east-1' }); 
+
+    function filtro(personas) {
+      return personas.edad >= 20;
+    }
+    const res = event.personas.filter(filtro);
+    console.log(res);
+    return eventBridge.putEvents({
+      
+      Entries: [
+        {
+          EventBusName: 'test',
+          Source: 'mensajeTest',
+          DetailType: 'UserSignUp',
+          Detail: JSON.stringify({"personas": res})
+      },
+      ]
+    }).promise()
+   }
+  
+} catch (error) {
+  console.log(error.message)
+}
